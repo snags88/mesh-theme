7c0c80fe2dfbe17a8e6425c566134b339e65d03e
Seiji Naganuma <Snaganumas@gmail.com>
2015-01-15 17:52:17 -0800

    add hyperlink on images

--- content-image.php
+++ content-image.php
@@ -12,8 +12,11 @@ $category = get_the_category()
 <?php if( has_post_thumbnail() ) { ?>
 	<div class="post-media">
 		<?php if( !is_singular() ) { ?>
-			<div class="hover-image"><span><?php echo $category[0]->cat_name; ?></span>
-				<?php the_post_thumbnail(); ?>
+			<div class="hover-image">
+				<a href="http://instagram.com/lookmachine" target "_blank">
+					<span><?php echo $category[0]->cat_name; ?></span>
+						<?php the_post_thumbnail(); ?>
+				</a>
 			</div>
 		<?php } else {
 			the_post_thumbnail('full');
